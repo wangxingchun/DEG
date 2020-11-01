d5855e02e5e42b9e46a124118f4113300ca701e7
xingchunwang <xingchunwang@gmail.com>
2020-11-01 18:07:53 +0800

    20

---
+++ YangziWeb/src/main/java/com/hpe/YangziWebApplication.java
@@ -0,0 +1,14 @@
+package com.hpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangziWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangziWebApplication.class, args);
+		System.out.println();
+	}
+
+}
