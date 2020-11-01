c976325aa6f2128e13d17eb6a06e82bda4b430b5
xingchunwang <xingchunwang@gmail.com>
2020-11-01 16:55:32 +0800

    20

---
+++ notification_deg/src/main/java/notification_deg/notification_deg/App.java
@@ -0,0 +1,14 @@
+package notification_deg.notification_deg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("OK");
+    }
+}
